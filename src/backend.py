c89adb2adbf1592d8d7e5fb07f0cf9c8e568b4bc
Ege Emir Özkan <egeemirozkan24@gmail.com>
2021-04-12 14:51:25 +0300

    Add a welcome message.

--- src/backend.py
+++ src/backend.py
@@ -5,3 +5,17 @@ from flask import Flask
 
 
 app = Flask(__name__) # Create the app object.
+
+
+@app.route('/')
+def hello_world():
+    return """
+    <html>
+    <head>
+    <title>MBS Backend</title>
+    </head>
+    <body>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/_GY8nN105xE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </body>
+    </html>
+    """
