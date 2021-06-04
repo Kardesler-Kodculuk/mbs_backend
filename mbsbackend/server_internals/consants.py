6a402b97d158e97043cf39f97a53d70f19d7d3ce
Ege Emir Özkan <egeemirozkan24@gmail.com>
2021-06-04 15:25:09 +0300

    Increase version number.

--- mbsbackend/server_internals/consants.py
+++ mbsbackend/server_internals/consants.py
@@ -1,4 +1,4 @@
 forbidden_fields = {"Student": ["password", "user_id", "student_id"],
                     "StudentAdvisor": ["password", "user_id", "student_id", "email"]}  # Fields forbidden to be
-version_number = "v0.2.2"
+version_number = "v0.2.3"
 # Changed by client.
